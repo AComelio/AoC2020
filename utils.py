e0f01b789db789e3bbc67470cd6dc3aa9a5f4e07
Adam Comelio <adam.comelio@arup.com>
2020-12-03 10:07:09 +0000

    Formatting

--- utils.py
+++ utils.py
@@ -12,7 +12,9 @@ def time_me(func):
         st = time.process_time_ns()
         pst = time.perf_counter_ns()
         r = func(*args, **kwargs)
-        print('Function done in {} nanoseconds process time'.format((time.process_time_ns()-st)))
-        print('Function done in {} seconds real time'.format((time.perf_counter_ns()-pst)/1e9))
+        print('Function done in {} nanoseconds process time'.format(
+                                            (time.process_time_ns()-st)))
+        print('Function done in {} seconds real time'.format(
+                                            (time.perf_counter_ns()-pst)/1e9))
         return r
     return inner1
