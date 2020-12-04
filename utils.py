b4b5004662cca6643483a6b36c83fb5c353999fd
Adam Comelio <adam.comelio@arup.com>
2020-12-04 15:00:19 +0000

    Removed process time logging

--- utils.py
+++ utils.py
@@ -9,12 +9,9 @@ import time
 
 def time_me(func):
     def inner1(*args, **kwargs):
-        st = time.process_time_ns()
-        pst = time.perf_counter_ns()
+        st = time.perf_counter_ns()
         r = func(*args, **kwargs)
-        print('Function done in {} nanoseconds process time'.format(
-                                            (time.process_time_ns()-st)))
         print('Function done in {} seconds real time'.format(
-                                            (time.perf_counter_ns()-pst)/1e9))
+                                            (time.perf_counter_ns()-st)/1e9))
         return r
     return inner1
