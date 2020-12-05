ed0fae1e2aa7fe07152738391f5072cd7e937e51
AComelio <48688112+AComelio@users.noreply.github.com>
2020-12-05 11:28:10 +0000

    Day 5 solution, added input tokeniser to utils

--- utils.py
+++ utils.py
@@ -15,3 +15,8 @@ def time_me(func):
                                             (time.perf_counter_ns()-st)/1e9))
         return r
     return inner1
+
+def tokenise_input_file(filepath, seperator='\n'):
+    with open(filepath, 'r') as f:
+        text = f.read()
+    return text.split(seperator)
